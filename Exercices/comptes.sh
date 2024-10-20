f60792a957ae3a1861b4332b5122366d9207b6ac
yangbo <yangbo.;lionel@qq.com>
2024-10-21 01:13:34 +0200

    Exercice 4

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 
-echo "Le nombre de Locations est "
-grep "Location" ~/2024-2025/PPE1/exercice1/Exercice1/ann/*/*/*.ann | wc -l
+total=0
+for i in 2016 2017 2018
+do
+	total=$(expr $total + $(grep "Location" ~/2024-2025/PPE1/exercice1/Exercice1/ann/$i/*/*.ann | wc -l))
+done
+echo "$total"
