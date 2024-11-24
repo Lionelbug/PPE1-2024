bbb6ee824f639e3b0af52afac315cc9bc28c1909
yangbo <yangbo.;lionel@qq.com>
2024-11-24 23:30:00 +0100

    Ajout du script

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -5,6 +5,22 @@ then
 	exit 1
 fi
 
+echo '<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+    </head>
+    <body>' >> "tableau-fr.html"
+echo -e '       <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+            <tr class="is-link">
+                <th>Number</th>
+                <th>URL</th>
+                <th>Code</th>
+                <th>Encoding</th>
+                <th>Word Number</th>
+            </tr>' >> "tableau-fr.html"
+
 file_path=$1
 line_number=1
 while read -r line
@@ -14,6 +30,14 @@ do
     encoding=${encoding:-"N/A"}
     nbmot=$(lynx -dump -nolist $line | wc -w)
 
-	echo -e "$line_number\t$line\t$http_code\t$encoding\t$nbmot" >> "tableau-fr.tsv"
+	echo -e "           <tr>
+                <td>$line_number</td>
+                <td><a href="$line">$line</a></td>
+                <td>$http_code</td>
+                <td>$encoding</td>
+                <td>$nbmot</td>" >> "tableau-fr.html"
 	line_number=$(expr $line_number + 1)
-done < "$file_path"
+done < $file_path
+echo '      </table>' >> "tableau-fr.html"
+echo '  </body>  
+</html>' >> "tableau-fr.html"
